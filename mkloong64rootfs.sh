ddb931c4bf8dc7be674c5aae44a0c9dd92ab27f0
Zhou Qiankang <wszqkzqk@qq.com>
2024-12-29 00:13:47 +0800

    feat: remove electron patch cleaning script since it's ineffective
    Signed-off-by: Zhou Qiankang <wszqkzqk@qq.com>

--- mkloong64rootfs.sh
+++ mkloong64rootfs.sh
@@ -17,19 +17,23 @@ yes y | sudo pacman -Scc --sysroot "$ROOTFS_DIR"
 
 echo "Compressing rootfs to tar.zst..."
 # Use pipe instead of buitin compression in bsdtar for better control over compression options
+# Upstream's release has a top directory `rootfs.$CARCH` so we also need this
 sudo bsdtar --create \
     --xattrs --acls \
-     -C "$ROOTFS_DIR/" . \
-    | zstd -c -T0 -15 - > \
-    "archlinux-bootstrap-$(date +%Y.%m.%d)-loong64.tar.zst"
+    -C "$(dirname "$ROOTFS_DIR")" \
+    "$(basename "$ROOTFS_DIR")" \
+    | zstd -c -f -T0 -15 - \
+    -o "archlinux-bootstrap-$(date +%Y.%m.%d)-loong64.tar.zst"
 sha512sum "archlinux-bootstrap-$(date +%Y.%m.%d)-loong64.tar.zst" > "archlinux-bootstrap-$(date +%Y.%m.%d)-loong64.tar.zst.sha512"
 
 echo "Compressing rootfs to squashfs..."
+# Uncomment -keep-as-directory will include top directory `rootfs.loong64`
 sudo mksquashfs \
-    "$ROOTFS_DIR/" \
+    "$ROOTFS_DIR" \
     "archlinux-bootstrap-$(date +%Y.%m.%d)-loong64.sfs" \
+    -noappend \
     -comp zstd \
-    -Xcompression-level 15
+    -Xcompression-level 15 #-keep-as-directory
 sha512sum "archlinux-bootstrap-$(date +%Y.%m.%d)-loong64.sfs" > "archlinux-bootstrap-$(date +%Y.%m.%d)-loong64.sfs.sha512"
 
 echo "Clean up rootfs directory..."
